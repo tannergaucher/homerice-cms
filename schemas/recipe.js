301dbb6feb5bad6d03f7d85f75dea15bf29bbbc5
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2021-03-22 18:33:41 -0400

    Update recipe.js

--- schemas/recipe.js
+++ schemas/recipe.js
@@ -1,75 +1,72 @@
 export default {
-  name: "recipe",
-  title: "Recipe",
-  type: "document",
+  name: 'recipe',
+  title: 'Recipe',
+  type: 'document',
   fields: [
     {
-      name: "title",
-      title: "Title",
-      type: "string",
+      name: 'title',
+      title: 'Title',
+      type: 'string',
     },
     {
-      name: "subtitle",
-      title: "Subtitle",
-      type: "string",
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'string',
     },
     {
-      name: "slug",
-      title: "Recipe Slug",
-      type: "slug",
+      name: 'draft',
+      title: 'Draft',
+      type: 'boolean',
+    },
+    {
+      name: 'slug',
+      title: 'Recipe Slug',
+      type: 'slug',
       options: {
-        source: "title",
+        source: 'title',
         maxLength: 96,
       },
     },
     {
-      name: "ingredients",
-      title: "Ingredients",
-      type: "array",
-      of: [{ type: "recipeIngredient" }],
-    },
-
-    {
-      name: "category",
-      title: "Category",
-      type: "reference",
-      to: { type: "category" },
+      name: 'ingredients',
+      title: 'Ingredients',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'ingredient' } }],
     },
     {
-      name: "mainImage",
-      title: "Main image",
-      type: "image",
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: { type: 'category' },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
       options: {
         hotspot: true,
       },
     },
     {
-      name: "youtubeVideoId",
-      title: "Youtube video ID",
-      type: "string",
+      name: 'youtubeVideoId',
+      title: 'Youtube video ID',
+      type: 'string',
     },
     {
-      name: "body",
-      title: "Recipe Body",
-      type: "blockContent",
+      name: 'body',
+      title: 'Recipe Body',
+      type: 'blockContent',
     },
     {
-      name: "publishedAt",
-      title: "Published at",
-      type: "datetime",
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
     },
   ],
-
   preview: {
     select: {
-      title: "title",
-      media: "mainImage",
+      title: 'title',
+      media: 'mainImage',
     },
-    //     prepare(selection) {
-    //       const { author } = selection;
-    //       return Object.assign({}, selection, {
-    //         subtitle: author && `by ${author}`,
-    //       });
-    //     },
   },
-};
+}
