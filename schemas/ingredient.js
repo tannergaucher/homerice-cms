a16a39b7240dfaad073982cb6b09e61980ee3562
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2021-03-29 14:24:49 -0400

    Update ingredient.js

--- schemas/ingredient.js
+++ schemas/ingredient.js
@@ -1,22 +1,27 @@
 export default {
-  name: "ingredient",
-  title: "Ingredient",
-  type: "document",
+  name: 'ingredient',
+  title: 'Ingredient',
+  type: 'document',
   fields: [
     {
-      name: "text",
-      title: "Text",
-      type: "string",
+      name: 'text',
+      title: 'Text',
+      type: 'string',
     },
     {
-      name: "ASIN",
-      title: "ASIN",
-      type: "string",
+      name: 'ASIN',
+      title: 'ASIN',
+      type: 'string',
+    },
+    {
+      name: 'externalHref',
+      title: 'External Href',
+      type: 'url',
     },
   ],
   preview: {
     select: {
-      title: "text",
+      title: 'text',
     },
   },
-};
+}
